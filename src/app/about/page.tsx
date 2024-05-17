67c0164591a8a7ed1c4802a6e7ed7e346fc3c926
Christian Dover <christiandover1@gmail.com>
2024-05-16 18:03:19 -0600

    Deploying to vercel

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from '@/components/layout/navbar';
 
-const AboutPage: React.FC = () => {
+// const AboutPage: React.FC = () => {
+export default function AboutPage(){
     return (
         <div>
-            <Navbar />
+            {/* <Navbar /> */}
             <h1>About Page</h1>
             <p>Welcome to the About page!</p>
         </div>
     );
 };
 
-export default AboutPage;
+// export default AboutPage;

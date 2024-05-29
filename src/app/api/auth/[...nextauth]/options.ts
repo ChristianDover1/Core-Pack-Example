0f1fd358d24fbe43563a5c2b3cc9e27736b28f04
Christian Dover <christiandover1@gmail.com>
2024-05-29 12:41:20 -0600

    updated database and created jobs feed component

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,7 @@
 
 import type {NextAuthOptions} from 'next-auth'
 import CredentialsProviders from 'next-auth/providers/credentials'
-import {getUser} from '@/utils/db/DB'
+import {getEmployee} from '@/utils/db/DB'
 
 export const options: NextAuthOptions = {
     providers: [
@@ -10,27 +10,61 @@ export const options: NextAuthOptions = {
         //     clientSecret: process.env.GITHUB_SECRET as string
         // }),
         CredentialsProviders({
-            name: "Credentials",
+            name: "Customer Login",
             credentials: {
-                username: {label: "Username:", type: "text", placeholder: "username"},
-                password: {label: "Password:", type: "password", placeholder: "password"}
+                email: {label: "email:", type: "text", placeholder: "Email"},
+                password: {label: "Password:", type: "password", placeholder: "Password"}
             },
             // get data from db
-            async authorize(credentials) {
+            async authorize(credentials, req) {
                 if (!credentials) {
                     return null
                 }
-                // const user = getUser(credentials.username)
-                const user = {id: "1", email:"John@corepack.com", name: "John", password: "password"}
+                var user = await getEmployee(credentials.email) as any
+                user = user[0]
+                // user.finally(() => {})
+                // const user = {id: "1", email:"John@corepack.com", name: "John", password: "password"}
                 // const user = {id: "1", name: "Customer1", password: "Customer1"}
-
-                if (credentials?.username === user.name && credentials?.password === "password") {
+                // return user.then((res: any) => {
+                // console.log(res)
+                console.log(credentials?.email === user.email , credentials?.password === user.password)
+                if (credentials?.email === user.email && credentials?.password === user.password) {
                     // return Promise.resolve(user)
                     return user
                 } else {
                     // return Promise.resolve(null)
                     return null
                 }
+                // })
+            }
+        }),
+        CredentialsProviders({
+            name: "Employee Login",
+            credentials: {
+                email: {label: "email:", type: "text", placeholder: "Email"},
+                password: {label: "Password:", type: "password", placeholder: "Password"}
+            },
+            // get data from db
+            async authorize(credentials, req) {
+                if (!credentials) {
+                    return null
+                }
+                var user = await getEmployee(credentials.email) as any
+                user = user[0]
+                // user.finally(() => {})
+                // const user = {id: "1", email:"John@corepack.com", name: "John", password: "password"}
+                // const user = {id: "1", name: "Customer1", password: "Customer1"}
+                // return user.then((res: any) => {
+                // console.log(res)
+                console.log(credentials?.email === user.email , credentials?.password === user.password)
+                if (credentials?.email === user.email && credentials?.password === user.password) {
+                    // return Promise.resolve(user)
+                    return user
+                } else {
+                    // return Promise.resolve(null)
+                    return null
+                }
+                // })
             }
         })
     ],
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
+// import JobsFeed from "@/components/jobs";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>
+    {/* <JobsFeed /> */}
+    {children}
+    </SessionProvider>;
 }
